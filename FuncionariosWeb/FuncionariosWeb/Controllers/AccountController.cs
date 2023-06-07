74248a342863e62456372ebe79523edefe123fdd
Danilo <danilo.silva@msn.com>
2023-06-07 19:30:47 -0300

    Usando o Identity de cabo a rabo - VII

--- FuncionariosWeb/FuncionariosWeb/Controllers/AccountController.cs
+++ FuncionariosWeb/FuncionariosWeb/Controllers/AccountController.cs
@@ -61,4 +61,25 @@ public class AccountController : Controller
         await signInManager.SignOutAsync();
         return RedirectToAction("index", "home");
     }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await signInManager.PasswordSignInAsync(
+                model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("index", "home");
+            }
+            ModelState.AddModelError(string.Empty, "Login Inválido");
+        }
+        return View(model);
+    }
 }
